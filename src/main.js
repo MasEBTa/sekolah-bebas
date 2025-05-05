006522c881c14e4bde5f31a062f0be47218456b8
MasEBTA <ebta.sb@gmail.com>
2025-05-05 15:03:17 +0800

    install plugin untuk github-pages dan pengaturan aouto deploy

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import VueStrokeWriter from "vue-stroke-writer";
-
 import { createPinia } from "pinia";
 
 const app = createApp(App);
@@ -20,8 +18,5 @@ const pinia = createPinia();
 app.use(router);
 // gunakan pinia di app
 app.use(pinia);
-
-// vua-stroke-writter
-app.use(VueStrokeWriter);
 
 app.mount("#app");
